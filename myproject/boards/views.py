48d3cbc6abfa6ec7a2f20a23bb3bc8ac422f309e
OAbouHajar <smsm.sy@hotmail.com>
2019-01-11 08:32:36 +0000

    after tutorial 2

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Board
+from django.http import Http404
+
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
-
-
 from django.conf.urls import url
-
+from django.contrib import admin
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    path('admin/', admin.site.urls),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^admin/', admin.site.urls),
 ]
