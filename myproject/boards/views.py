cf11460f1f8863f91d98ec18b9d84164ab7b6aa2
OAbouHajar <smsm.sy@hotmail.com>
2019-01-22 16:18:38 +0000

    after tut 4

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from django.http import HttpResponse
-from .models import Board
+from .models import Board, Topic, Post
 from django.http import Http404
+from .forms import NewTopicForm
+from django.contrib.auth.decorators import login_required
+
 
 
 def home(request):
@@ -10,8 +14,26 @@ def home(request):
 
 
 def board_topics(request, pk):
-    try:
-        board = Board.objects.get(pk=pk)
-    except Board.DoesNotExist:
-        raise Http404
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+
+@login_required
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = request.user
+            topic.save()
+            Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=request.user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
